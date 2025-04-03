ecf5d70a6ed9da5840eb2a1d6a18bbe2c4ddf2a2
Carlo Aguirre <carlo_aguirre@outlook.cl>
2025-04-03 16:44:56 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+
+	frutas_favoritas = ['manzana', 'coco', 'tuna']
+	contexto = {"nombre": "Carlo", "apellido": "Aguirre", "frutas": frutas_favoritas}
+	return render(request, 'core/home.html', contexto)
