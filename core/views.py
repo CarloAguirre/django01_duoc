90464403e78abb4bf103a12dc01a0eec2ad91434
Carlo Aguirre <carlo_aguirre@outlook.cl>
2025-04-03 22:20:35 -0300

    home, animales added

--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-
 	frutas_favoritas = ['manzana', 'coco', 'tuna']
 	contexto = {"nombre": "Carlo", "apellido": "Aguirre", "frutas": frutas_favoritas}
 	return render(request, 'core/home.html', contexto)
+
+def animales(request):
+	return render(request, 'core/animales.html')
