b6ba785ab8f4b8b3cec0413b14e10de44f6f1675
Carlo Aguirre <carlo_aguirre@outlook.cl>
2025-04-07 20:07:37 -0400

    slider works

--- core/views.py
+++ core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-	frutas_favoritas = ['manzana', 'coco', 'tuna']
-	contexto = {"nombre": "Carlo", "apellido": "Aguirre", "frutas": frutas_favoritas}
-	return render(request, 'core/home.html', contexto)
+	return render(request, 'core/home.html')
 
 def animales(request):
 	return render(request, 'core/animales.html')
 
 def lugares(request):
 	return render(request, 'core/Lugarestf.html')
+
+def logros(request):
+	return render(request, 'core/Logros.html')
+
+def mi_cuenta(request):
+	return render(request, 'core/micuentatf.html')
+
+def forowiki(request):
+	return render(request, 'core/forowiki.html')
